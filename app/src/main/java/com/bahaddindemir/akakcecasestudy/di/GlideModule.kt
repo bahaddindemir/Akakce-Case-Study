fa70295795dd73b72607d25bce255806c5ceb1d0
Bahaddin Demir <demirbahaddin@hotmail.com>
2025-03-20 18:59:33 +0300

    Kotlin and Gradle version updated. KSP support added.

--- app/src/main/java/com/bahaddindemir/akakcecasestudy/di/GlideModule.kt
+++
@@ -1,7 +0,0 @@
-package com.bahaddindemir.akakcecasestudy.di
-
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-
-@GlideModule
-class GlideModule : AppGlideModule()
--- app/src/main/java/com/bahaddindemir/akakcecasestudy/ui/binding/ViewHolderBinding.kt
+++ app/src/main/java/com/bahaddindemir/akakcecasestudy/ui/binding/ViewHolderBinding.kt
@@ -4,12 +4,12 @@ import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bahaddindemir.akakcecasestudy.R
-import com.bahaddindemir.akakcecasestudy.di.GlideApp
+import com.bumptech.glide.Glide
 
 @BindingAdapter("bindingProductImageUrl")
 fun bindingProductImageUrl(imageView: ImageView, path: String?) {
   path?.let {
-    GlideApp.with(imageView.context)
+    Glide.with(imageView.context)
             .load(it)
             .error(ContextCompat.getDrawable(imageView.context, R.drawable.ic_launcher_foreground))
             .into(imageView)
